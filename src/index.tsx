44fdcdbe6ce476b63c887b08aa707f6102404707
unique.mo <myx3901051@qq.com>
2020-04-23 16:38:12 +0800

    feat: introduce blueprints react component library

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './pages/App'
 
+import 'normalize.css/normalize.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
+
 const Root = () => {
   return (
     <div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Button } from '@blueprintjs/core'
+import style from './app.module.css'
 
 const App = () => {
   return (
-    <div>app</div>
+    <div className={style.app}>
+      HHH
+      <Button>abc</Button>
+    </div>
   )
 }
 
