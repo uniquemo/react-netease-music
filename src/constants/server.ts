03aef4a13b28e27e9df1560ea32df48f0221f821
unique.mo <myx3901051@qq.com>
2020-05-15 19:03:49 +0800

    feat: add api address

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,4 +1,4 @@
 const serverEnv = process.env.NODE_ENV || 'development'
-const serverAddr = serverEnv === 'development' ? 'localhost:3000' : ''
+const serverAddr = serverEnv === 'development' ? 'localhost' : '47.115.57.59'
 
-export const SERVER = `http://${serverAddr}`
+export const SERVER = `http://${serverAddr}:3000`
