0bf4ba5b62dc57e2425d99f91036235f6f404037
unique.mo <myx3901051@qq.com>
2021-06-20 11:13:22 +0800

    chore: add /api and /graphql proxy to webpack dev server

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,5 +1,7 @@
 const LOCAL_ORIGIN = 'http://localhost'
 const REMOTE_ORIGIN = 'http://www.uniquemo.cn'
 
-export const SERVER = __LOCALHOST__ ? `${LOCAL_ORIGIN}:3000` : `${REMOTE_ORIGIN}/api`
-export const GRAPHQL_SERVER = __LOCALHOST__ ? `${LOCAL_ORIGIN}:4000/graphql` : `${REMOTE_ORIGIN}/graphql`
+export const PORT = 8080
+
+export const SERVER = __LOCALHOST__ ? `${LOCAL_ORIGIN}:${PORT}/api` : `${REMOTE_ORIGIN}/api`
+export const GRAPHQL_SERVER = __LOCALHOST__ ? `${LOCAL_ORIGIN}:${PORT}/graphql` : `${REMOTE_ORIGIN}/graphql`
